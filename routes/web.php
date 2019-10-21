37123383a6723b81677a2f36f3dbb8d5f0c33dda
= <=>
2019-10-21 11:04:57 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,22 @@ Route::prefix('linh-vuc')->group(function(){
 	Route::get('danh-sach/{id}/edit', 'LinhVucController@edit')->name('linh-vuc.danh-sach.chinh-sua');
 	Route::get('danh-sach/{id}/d','LinhVucController@UpdateStatus')->name('linh-vuc.danh-sach.xoa');
 });
+// code khiem
+
+Route::get('/index/Register', function () {
+    return view('Register');
+});
+Route::get('/index/login', function(){
+	return view('Login');
+});
+Route::get('/index/Database',function(){
+	return view('Database');
+});
+Route::get('/index/Database','LinhVucController@data');
+Route::prefix('linh-vuc')->group(function(){
+	Route::get('them-moi','LinhVucController@create')->name('linh-vuc.them-moi');
+	Route::post('them-moi','LinhVucController@store')->name('linh-vuc.xl-them-moi');
+});
 
 
 
