3732c64e315a164e67781e3e0a7e307e347f99fa
= <=>
2019-10-21 11:08:05 +0700

    new

---
+++ resources/views/form-linh-vuc.blade.php
@@ -0,0 +1,18 @@
+@extends('ViewLinhVuc')
+@section('main-content')
+<div class="row" >
+   <div class="col-lg-6">
+   	<div class="card">
+   		<div class="card-body">
+	  <h4 class="mb-3 header-title">Thêm Mới Lĩnh Vực</h4>
+   <form action="{{route('linh-vuc.xl-them-moi')}}" method="POST">
+   	@csrf
+       <div class="form-group mb-3">
+          <label for="ten_linh_vuc">Tên Lĩnh Vực</label>
+          <input type="text" id="ten_linh_vuc" name="ten_linh_vuc"class="form-control" placeholder="Tên Lĩnh Vực">          
+           </div>
+            <button type="submit" class="btn btn-primary waves-effect waves-light">Lưu</button>
+               </form>
+      </div>
+</div>
+@endsection 
