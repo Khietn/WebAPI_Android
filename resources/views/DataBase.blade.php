4f336c17c0461efaa2aa0efb9b1819d54ac323e4
votienkhiem <votienkhiem@users.noreply.github.com>
2019-10-28 09:57:10 +0700

    edit

--- resources/views/DataBase.blade.php
+++ resources/views/DataBase.blade.php
@@ -767,7 +767,7 @@
                             
                                           <!-- them bang o day -->                                                                                                                                                                                                                     
                                     </tbody>
-                                   
+                                    <button class="btn btn-dark waves-effect waves-light" style="margin-bottom:20px"><a href="{{route('linh-vuc.them-moi')}}">Thêm</a></button>
                                         </table>
                                         
                                     </div> <!-- end .table-responsive -->
