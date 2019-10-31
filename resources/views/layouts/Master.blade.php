c811529209774d34e561cbfa9a1b226c797d8f77
votienkhiem <votienkhiem@users.noreply.github.com>
2019-10-31 20:58:48 +0700

    edit pages master

--- resources/views/layouts/Master.blade.php
+++ resources/views/layouts/Master.blade.php
@@ -1,11 +1,6 @@
 
-<<<<<<< HEAD
-@section('header')
-=======
->>>>>>> 44f9e738b9c2d970d9fc35d1926d20e7781f877c
 <!DOCTYPE html>
 <html lang="en">
-
     <head>
         <meta charset="utf-8" />
         <title>@yield('title')</title>
@@ -23,9 +18,7 @@
         <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
         <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
         <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
-
     </head>
-
     <body>
         <!-- Navigation Bar-->
         <header id="topnav">
@@ -46,7 +39,6 @@
                             </a>
                             <!-- End mobile menu toggle-->
                         </li>
-
                         <li class="d-none d-sm-block">
                             <form class="app-search">
                                 <div class="app-search-box">
@@ -217,139 +209,7 @@
                                 <img src="{{asset('assets/images/logo-sm.png')}}" alt="" height="28">
                             </span>
                         </a>
-                    </div>
-
-                    <ul class="list-unstyled topnav-menu topnav-menu-left m-0">
-            
-                        <li class="dropdown d-none d-lg-block">
-                            <a class="nav-link dropdown-toggle waves-effect" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                                Reports
-                                <i class="mdi mdi-chevron-down"></i> 
-                            </a>
-                            <div class="dropdown-menu">
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    Finance Report
-                                </a>
-
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    Monthly Report
-                                </a>
-
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    Revenue Report
-                                </a>
-
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    Settings
-                                </a>
-
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    Help & Support
-                                </a>
-
-                            </div>
-                        </li>
-
-                        <li class="dropdown dropdown-mega d-none d-lg-block">
-                            <a class="nav-link dropdown-toggle waves-effect" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                                Mega Menu
-                                <i class="mdi mdi-chevron-down"></i> 
-                            </a>
-                            <div class="dropdown-menu dropdown-megamenu">
-                                <div class="row">
-                                    <div class="col-sm-8">
-                            
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                <h5 class="text-dark mt-0">UI Components</h5>
-                                                <ul class="list-unstyled megamenu-list mt-2">
-                                                    <li>
-                                                        <a href="javascript:void(0);">Widgets</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Nestable List</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Range Sliders</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Masonry Items</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Sweet Alerts</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Treeview Page</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Tour Page</a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-
-                                            <div class="col-md-4">
-                                                <h5 class="text-dark mt-0">Applications</h5>
-                                                <ul class="list-unstyled megamenu-list mt-2">
-                                                    <li>
-                                                        <a href="javascript:void(0);">Email Pages</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Profile</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Calendar</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Team Contacts</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Maintenance</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Coming Soon Page</a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-
-                                            <div class="col-md-4">
-                                                <h5 class="text-dark mt-0">Layouts</h5>
-                                                <ul class="list-unstyled megamenu-list mt-2">
-                                                    <li>
-                                                        <a href="javascript:void(0);">Small Sidebar</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Light Sidebar</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Dark Topbar</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Preloader</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0);">Sidebar Collapsed</a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-4">
-                                        <div class="text-center mt-3">
-                                            <h3 class="text-dark">Launching Discount Sale!</h3>
-                                            <p class="font-16">Save up to 55% off.</p>
-                                            <button class="btn btn-primary mt-1">Download Now <i class="mdi mdi-arrow-right-bold-outline ml-1"></i></button>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </li>
-                    </ul>
+                    </div>    
                     <div class="clearfix"></div>
                 </div>
             </div>
@@ -415,7 +275,6 @@
                                     </li>
                                 </ul>
                             </li>
-
                         </ul>
                         <!-- End navigation menu -->
 
