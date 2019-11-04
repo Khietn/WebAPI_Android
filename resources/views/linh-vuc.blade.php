a003affa51745179d361245f798a12d0e53061b6
votienkhiem <votienkhiem@users.noreply.github.com>
2019-11-04 14:43:43 +0700

    edit

--- resources/views/linh-vuc.blade.php
+++ resources/views/linh-vuc.blade.php
@@ -42,38 +42,7 @@ Quản Lý Lĩnh Vực
                                     </tbody>
                                    <!-- <button class="btn btn-dark waves-effect waves-light"  style="margin-bottom:20px"><a href="{{route('linh-vuc.them-moi')}}">Thêm</a></button> -->
                                     <button type="button" class="btn btn-success waves-effect waves-light" data-toggle="modal"  style="margin-bottom:20px" data-target="#con-close-modal">Thêm</button>
-                                    <!-- modalthemlinhvuc -->
-                                    <!-- modal sửa -->
-                                    <div id="con-close-modal-sua" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h4 class="modal-title">Sửa Lĩnh Vực</h4>
-                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                                        </div>
-                                        <div class="modal-body p-4">                                   
-                                            <div class="row">
-                                                <div class="col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="field-3" class="control-label">Tên Lĩnh Vực</label>
-                                                        <input type="text" class="form-control" id="field-3" placeholder="Tên Lĩnh Vực">
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label for="field-3" class="control-label">Tên Lĩnh Vực Mới</label>
-                                                        <input type="text" class="form-control" id="field-3" placeholder="Tên Lĩnh Vực Mới">
-                                                    </div>
-                                                </div>
-                                            </div>                                                                                     
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-info waves-effect waves-light">Lưu</button>
-                                            <button type="button" class="btn btn-danger waves-effect" data-dismiss="modal">Hủy</button>                                         
-                                        </div>
-                                    </div>
-                                </div>
-                            </div><!-- /.modal -->
-
-                                    <!-- kết thúc modal sửa -->
+                            <!-- modalthemlinhvuc -->                                                                
                                     <form action="{{route('linh-vuc.xl-them-moi')}}" method="POST">
                                  	@csrf
                              <div id="con-close-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
@@ -100,9 +69,42 @@ Quản Lý Lĩnh Vực
                                     </div>
                                 </div>
                             </div><!-- /.ketthucmodalthem -->
-
                             </form>
-                                    <!-- kethucmodalthemlinhvuc -->
+                            <!-- kethucmodalthemlinhvuc -->
+                            
+                            <!-- modal sửa -->
+                                 <form action="#" method="POST" class="modal edit">
+                                 @csrf
+                                 <div id="con-close-modal-sua" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title">Sửa Lĩnh Vực</h4>
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                        </div>
+                                        <div class="modal-body p-4">                                   
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label for="ten_linh_vuc" class="control-label">Tên Lĩnh Vực</label>
+                                                        <input type="text" class="form-control" id="ten_linh_vuc" placeholder="Tên Lĩnh Vực">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="field-3" class="control-label">Tên Lĩnh Vực Mới</label>
+                                                        <input type="text" class="form-control" id="field-3" placeholder="Tên Lĩnh Vực Mới">
+                                                    </div>
+                                                </div>
+                                            </div>                                                                                     
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-info waves-effect waves-light">Lưu</button>
+                                            <button type="button" class="btn btn-danger waves-effect" data-dismiss="modal">Hủy</button>                                         
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- /.modal -->
+                            </form>
+                            <!-- kết thúc modal sửa -->
                                     <!-- btn tim kiem -->
                                      <ul class="list-unstyled topnav-menu float-right mb-0">
                                     <li class="d-none d-sm-block">
