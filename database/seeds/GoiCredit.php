ed29e5f3fa8ab3a0198b50af969d9e97e54660a7
Chanh Sau <dumo7894@gmail.com>
2019-12-08 20:32:43 +0700

    seeder

--- database/seeds/GoiCredit.php
+++ database/seeds/GoiCredit.php
@@ -11,6 +11,8 @@ class GoiCredit extends Seeder
      */
     public function run()
     {
-        DB::table('_goi_credit')->insert(['ten_goi'=>'Người mới', 'credit'=>100, 'so_tien'=>20000]);
+        DB::table('goi_credit')->insert(['ten_goi'=>'Người mới', 'credit'=>100, 'so_tien'=>20000]);
+        DB::table('goi_credit')->insert(['ten_goi'=>'Khuyến mãi', 'credit'=>200, 'so_tien'=>30000]);
+        DB::table('goi_credit')->insert(['ten_goi'=>'Giảm Giá', 'credit'=>400, 'so_tien'=>40000]);
     }
 }
