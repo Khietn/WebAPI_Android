2d72ebd3e56a413aac2abd6fbde6bf031d5f9cb2
TrungKhiet <khiet.nguyen2@gmail.com>
2019-12-12 21:43:48 +0700

    Them API

--- routes/api.php
+++ routes/api.php
@@ -15,37 +15,42 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/cau-hoi/{id}','CauHoiController@getAPI');
-Route::get('/cau-hoi','CauHoiController@indexAPI');
-Route::get('/nguoi-choi','NguoiChoiController@indexAPI');
+
+Route::get('/cau-hoi/{id}','CauHoiController@getAPI'); 
+Route::get('/cau-hoi','CauHoiController@getAPIe'); //api/cau-hoi?linh_vuc_id=.... lay theo linh vuc
+
+
+Route::get('/nguoi-choi','NguoiChoiController@getAPIa'); // "api/nguoi-choi?username=...&password=..." lay theo Username + password
 Route::get('/nguoi-choi/{id}','NguoiChoiController@getAPI');
+Route::post('/them-nguoi-choi','NguoiChoiController@storeAPI');
+Route::put('/chinh-sua-nguoi-choi','NguoiChoiController@editAPI');
 
-Route::get('/linh-vuc','LinhVucController@indexAPI');
-Route::get('/linh-vuc/{id}','LinhVucController@getAPI');
+Route::get('/linh-vuc','LinhVucController@indexAPI'); //Lay het tat ca linh vuc
+Route::get('/linh-vuc/{id}','LinhVucController@getAPI'); //Lay linh vuc theo ID
 
-Route::get('/goi-credit','GoiCreditController@indexAPI');
-Route::get('/goi-credit/{id}','GoiCreditController@getAPI');
+Route::get('/goi-credit','GoiCreditController@indexAPI');// Lay het tat ca goi-credit "api/goi-credit"
+Route::get('/id-goi-credit','GoiCreditController@getAPI'); //Lay goi credit theo ID
 
-Route::get('/chi-tiet','ChiTietLuotChoiController@indexAPI');
-Route::get('/chi-tiet/{id}','ChiTietLuotChoiController@getAPI');
+Route::get('/chi-tiet','ChiTietLuotChoiController@indexAPI');// Lay het tat ca chi tiet luot choi
+Route::get('/chi-tiet/{id}','ChiTietLuotChoiController@getAPI'); 
 
-Route::get('/cau-hinh-diem-cau-hoi','CauHinhDiemCauHoiController@indexAPI');
+Route::get('/cau-hinh-diem-cau-hoi','CauHinhDiemCauHoiController@indexAPI'); // Cau hinh diem cau hoi
 Route::get('/cau-hinh-diem-cau-hoi/{id}','CauHinhDiemCauTroiController@getAPI');
 
-Route::get('/cau-hinh-app','CauHinhAppController@indexAPI');
+Route::get('/cau-hinh-app','CauHinhAppController@indexAPI'); //Cau hinh app
 Route::get('/cau-hinh-app/{id}','CauHinhAppController@getAPI');
 
-Route::get('/cau-hinh-tro-giup','CauHinhTroGiupController@indexAPI');
+Route::get('/cau-hinh-tro-giup','CauHinhTroGiupController@indexAPI'); //Cau hinh tro giup
 Route::get('/cau-hinh-tro-giup/{id}','CauHinhTroGiupController@getAPI');
 
-Route::get('/chi-tiet-luot-choi','ChiTietLuotChoiController@indexAPI');
+Route::get('/chi-tiet-luot-choi','ChiTietLuotChoiController@indexAPI'); //
 Route::get('/chi-tiet-luot-choi/{id}','ChiTietLuotChoiController@getAPI');
 
 Route::get('/lich-su-mua-credit','LichSuMuaCreditController@indexAPI');
 Route::get('/lich-su-mua-credit/{id}','LichSuMuaCreditController@getAPI');
 
-Route::get('/luot-choi','LuotChoiController@indexAPI');
-Route::get('/luot-choi/{id}','LuotChoiController@getAPI');
+Route::get('/luot-choi','LuotChoiController@indexAPI'); 
+Route::get('/luot-choi/{id}','LuotChoiController@getAPI'); 
 
-Route::get('/quan-tri-vien','QuanTriVienController@indexAPI');
-Route::get('/quan-tri-vien/{id}','QuanTriVienController@getAPI');
+Route::get('/quan-tri-vien','QuanTriVienController@indexAPI'); 
+Route::get('/quan-tri-vien/{id}','QuanTriVienController@getAPI'); 
